0be026da285a9eb1a7de824268f9014c56d74474
Vassili Helou <vlhelou1971@hotmail.com>
2025-01-05 16:52:28 -0300

    pre cli

---
+++ Front/src/app/ic/ic-autocomplete/ic-autocomplete.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ic-autocomplete',
+  standalone: true,
+  imports: [],
+  templateUrl: './ic-autocomplete.component.html',
+  styleUrl: './ic-autocomplete.component.scss'
+})
+export class IcAutocompleteComponent {
+
+}
